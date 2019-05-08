281c7620f77fe00176a732a7d8f774edccec7d81
Vlad <vladpochta5@gmail.com>
2019-05-08 21:26:10 +0300

    OOP strategy

--- strategy.py
+++ strategy.py
@@ -23,7 +23,7 @@ class Order:
 
 
     def total(self):
-        if not hasattr(self, __total):
+        if not hasattr(self, '__total'):
             self.__total = sum(item.total() for item in self._items)
         return self.__total
 
@@ -35,6 +35,39 @@ class Order:
         return self.total() - discount
 
     def __repr__(self):
-        fmt = '<Order total: {:2f} due: {:2f}>'
+        fmt = '<Order total: {:.2f} due: {:.2f}>'
         return fmt.format(self.total(), self.due())
+
+
+class Promotion(ABC):
+
+    @abstractmethod
+    def discount(self, order):
+        """Return discount as a position dollar amount"""
+
+
+class FidelityPromo(Promotion):
+
+    def discount(self, order):
+        return order.total() * .05 if order.customer.fidelity >= 1000 else 0
+
+
+
+class BulkItemPromo(Promotion):
+
+    def discount(self, order):
+        discount = 0
+        for item in order._items:
+            discount += item.total() * .1 if item.quantity >= 20 else 0
+        return discount
+
+
+class LargeOrderPromo(Promotion):
+    """7% discount for orders with 10 or more distinct items"""
+
+    def discount(self, order):
+        distinct_items = {item.product for item in order._items}
+        if len(distinct_items) >= 10:
+            return order.total() * .07
+        return 0
 
