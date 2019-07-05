4274c0ceede9538aa196404016b62cd56e01dde5
Vladyslav <vmash@softserveinc.com>
2019-07-05 19:28:34 +0300

    Fib

--- 7_closures/simple_decorator.py
+++ 7_closures/simple_decorator.py
@@ -1,16 +1,20 @@
-from time import time
+import functools
+from time import perf_counter
+
 
 
 def examine(func):
+    @functools.wraps(func)
     def wrapper(*args, **kwargs):
-        started_at = time()
+        started = perf_counter()
         result = func(*args, **kwargs)
-        elapsed_time = time() - started_at
-        print(f'It took {elapsed_time}ms to execute {func.__name__}')
+        elapsed_time = perf_counter() - started
+        args_str = ','.join(str(arg) for arg in args)
         kwargs_str = ','.join((f'{key}={value}' for key, value in kwargs.items()))
-        print(f'Arguments: {args}, Keyword arguments: {kwargs_str}, result: {result}')
+        print(f'[{elapsed_time:.5f}s] {func.__name__}({args_str}, {kwargs_str}) -> {result}')
         return result
     return wrapper
+
 
 @examine
 def work(b, a):
@@ -19,4 +23,28 @@ def work(b, a):
     return 'Hello'
 
 
-work(1, a=2)
+@examine
+def factorial(n):
+    return n if n == 1 else n * factorial(n - 1)
+
+
+@examine
+def fib(n):
+    first, second = 0, 1
+    while (second < n):
+       first, second = second, first + second
+       print(first)
+
+
+@examine
+def fib2(n):
+    if n < 2:
+        return n
+    return fib2(n-2) + fib2(n-1)
+
+
+if __name__ == '__main__':
+    # work(1, a=2)
+    # print(factorial(5))
+    fib2(10)
+
