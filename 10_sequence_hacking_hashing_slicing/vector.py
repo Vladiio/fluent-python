bff6bf16554f52508f0c9be4fb03a0e498dd1377
Vladyslav <vmash@softserveinc.com>
2019-07-26 20:09:30 +0300

    Custom slicing

--- 10_sequence_hacking_hashing_slicing/vector.py
+++ 10_sequence_hacking_hashing_slicing/vector.py
@@ -1,6 +1,7 @@
 import reprlib
 import array
 import math
+import numbers
 
 
 class Vector:
@@ -12,6 +13,16 @@ class Vector:
     def __len__(self):
         return len(self._components)
 
+    def __getitem__(self, index):
+        cls = type(self)
+        if isinstance(index, slice):
+            return cls(self._components[index])
+        elif isinstance(index, numbers.Integral):
+            return self._components[index]
+        else:
+            msg = '{cls.__name__} indices must be integers'
+            raise TypeError(msg.format(cls=cls))
+
     def __iter__(self):
         return iter(self._components)
 
@@ -20,5 +31,25 @@ class Vector:
         components = components[components.find('['):-1]
         return 'Vector({})'.format(components)
 
+    def __str__(self):
+        return str(tuple(self._components))
 
+    def __bytes__(self):
+        return (bytes([ord(self.typecode)]) + bytes(self._components))
 
+    def __eq__(self, other):
+        return tuple(self) == tuple(other)
+
+    def __abs__(self):
+        return math.sqrt(sum(x * x for x in self))
+
+    def __bool__(self):
+        return bool(abs(self))
+
+    @classmethod
+    def frombytes(cls, octets):
+        typecode = chr(octets[0])
+        # import pdb
+        # pdb.set_trace()
+        memv = memoryview(octets[1:]).cast(typecode)
+        return cls(memv)
